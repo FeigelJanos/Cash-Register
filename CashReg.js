398fa0b8f56cd2c62fc093b5239755ae4c987d34
John Feigel <32643682+FeigelJanos@users.noreply.github.com>
2019-02-23 18:51:04 +0100

    Update CashReg.js

--- CashReg.js
+++ CashReg.js
@@ -1,5 +1,7 @@
 function checkCashRegister(price, cash, cid) {
   var change;
+  const currencyTable=[["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], 
+                       ["DOLLAR", 1], ["FIVE", 5], ["TEN", 10],	["TWENTY", 20],	["ONE HUNDRED", 100]];
   // Here is your change, ma'am.
   return change;
 }
